92493c8072e30c8309a27809c97224b23afc27ef
Daniel Spiewak <djspiewak@gmail.com>
2011-09-10 22:34:33 +0200

    Added a few transformers

--- transformer/src/main/scala/ftw/transformer/transformers.scala
+++ transformer/src/main/scala/ftw/transformer/transformers.scala
@@ -1,2 +1,23 @@
-package ftw.transformer
+package ftw
+package transformer
 
+trait Transformer[-A] {
+  def apply(a: A): Stream[Byte]
+}
+
+
+trait Transformable {
+  def toByteChunk[A : Transformer](a: A): Stream[Byte]
+}
+
+trait StringTransformable extends Transformable {
+  implicit object StringTransformer extends Transformer[String] {
+    def apply(str: String) = Stream(str.getBytes: _*)
+  }
+}
+
+trait NodeSeqTransformable extends Transformable {
+  implicit object NodeSeqTransformer extends Transformer[xml.NodeSeq] {
+    def apply(ns: xml.NodeSeq) = Stream(ns.toString.getBytes: _*)
+  }
+}
