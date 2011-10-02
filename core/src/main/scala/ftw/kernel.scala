df9e37184fae8f776291a888b119770651cf2e4b
Jon-Anders Teigen <mail@jteigen.com>
2011-10-02 17:27:14 +0200

    asInstanceOf[Disappointment] BE GONE! (composing partial functions instead)

--- core/src/main/scala/ftw/kernel.scala
+++ core/src/main/scala/ftw/kernel.scala
@@ -14,24 +14,18 @@ trait ResponderFactory[-A, +B, +R <: Responder[A, B]] {
   def responder: R
 }
 
-trait Matcher[-A] { matcher =>
+trait Matcher[-A] {
   def matches(a:A):Boolean 
   
   def handledBy[A2 <: A, B, R <: Responder[A2, B]](resp: ResponderFactory[A2, B, R]): Handled[A2, B, R#Env] =
-    new Handled[A2, B, R#Env](Seq(matcher -> resp.asInstanceOf[ResponderFactory[A2, B, Responder[A2, B] { type Env = R#Env }]]))
+    Handled[A2, B, R#Env]( e => {
+      case a if matches(a) => resp.asInstanceOf[ResponderFactory[A2, B, Responder[A2, B]{ type Env = R#Env}]].responder.respond(e)(a)
+    })
 }
 
-case class Handled[-A, +B, E](handled: Seq[(Matcher[A], ResponderFactory[A, B, Responder[A, B] { type Env = E }])]) extends (E => PartialFunction[A, B]){
-  private type Disappointment[A2, B2, E2] = Seq[(Matcher[A2], ResponderFactory[A2, B2, Responder[A2, B2] { type Env = E with E2 }])]
-   
+case class Handled[-A, +B, E](handled: E => PartialFunction[A, B]) extends (E => PartialFunction[A, B]){
   def ~[A2 <: A, B2 >: B, E2](other: Handled[A2, B2, E2]): Handled[A2, B2, E with E2] =
-    Handled[A2, B2, E with E2]((handled ++ other.handled).asInstanceOf[Disappointment[A2, B2, E2]])
+    Handled[A2, B2, E with E2]{ e => handled(e) orElse other(e) }
 
-  private object Responder {
-    def unapply(a:A) = handled.find(_._1.matches(a)).map(_._2.responder)
-  }
-
-  def apply(env:E) = {
-    case a@Responder(responder) => responder.respond(env)(a)
-  }
+  def apply(env:E) = handled(env)
 }
